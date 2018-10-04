7a28e1e7c7bd5302c83b8a018054b40620006b20
Mfweb <h@mfweb.pw>
2018-10-05 04:50:30 +0800

    移动修改密码页面位置

--- AdaoMemberSystem/pages/userMember/userMember.js
+++ AdaoMemberSystem/pages/userMember/userMember.js
@@ -46,6 +46,9 @@ Page({
       ShowCertMsg: false,//是否显示实名认证消息
       CopyLoading: false,//复制手机号loading
     },
+    changePasswdOpenData: {
+      CPLoading: false
+    },
     popupMenuOpenData: {
       show: false,
       statusBarHeight: app.globalData.SystemInfo.Windows.statusBarHeight,
@@ -126,6 +129,10 @@ Page({
       this.data.authOpenData.ShowCertMsg = false;
       this.setData({ authOpenData: this.data.authOpenData });
     }
+    else if (this.data.popupMenuOpenData.selectedIndex == 2){
+      this.setData({ pullDownRefing:false});
+      wx.stopPullDownRefresh();
+    }
   },
   /**
    * 切换页面
@@ -180,6 +187,52 @@ Page({
   onAuthClose: function() {
     this.data.authOpenData.CertFormShow = false;
     this.setData({ authOpenData: this.data.authOpenData });
+  },
+  /**
+   * 确认修改密码
+   */
+  onChangePasswdSubmit(e) {
+    var old_passwd = e.detail.value.opass;
+    var new_passwd = e.detail.value.npass;
+    var new_passwd2 = e.detail.value.npass2;
+    if (old_passwd < 5 || new_passwd < 5 || new_passwd2 < 5) {
+      app.showError('密码至少5位');
+      return;
+    }
+    if (new_passwd != new_passwd2) {
+      app.showError('两次输入不一致');
+      return;
+    }
+    if (this.data.changePasswdOpenData.CPLoading == true) return;
+    this.data.changePasswdOpenData.CPLoading = true;
+    this.setData({ changePasswdOpenData: this.data.changePasswdOpenData });
+
+    http.api_request(
+      app.globalData.ApiUrls.ChangePasswordURL,
+      {
+        oldpwd: old_passwd,
+        pwd: new_passwd,
+        repwd: new_passwd2
+      },
+      function (res) {
+        if (typeof res == 'object') {
+          if (res.status == 1)
+            app.logOut();
+          else
+            app.showError(res.info);
+        }
+        else {
+          app.showError("发生了错误");
+        }
+        this.data.changePasswdOpenData.CPLoading = false;
+        this.setData({ changePasswdOpenData: this.data.changePasswdOpenData });
+      }.bind(this),
+      function () {
+        app.showError('发生了错误');
+        this.data.changePasswdOpenData.CPLoading = false;
+        this.setData({ changePasswdOpenData: this.data.changePasswdOpenData });
+      }.bind(this)
+    );
   },
   /**
    * 确认实名认证
