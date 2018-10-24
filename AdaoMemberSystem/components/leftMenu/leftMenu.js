59aec25cdbc1ddd9218c586753cfcbf267e80ee4
Mfweb <h@mfweb.pw>
2018-10-24 11:08:21 +0800

    修复图片未载入时错误无法弹出问题

--- AdaoMemberSystem/components/leftMenu/leftMenu.js
+++ AdaoMemberSystem/components/leftMenu/leftMenu.js
@@ -71,7 +71,7 @@ Component({
     },
     onViewImage: function () {
       if (this.data.data.picURL == '' || this.data.data.picURL == null) {
-        app.showError('图片为空');
+        getApp().showError('图片为空');
         return;
       }
       wx.previewImage({
