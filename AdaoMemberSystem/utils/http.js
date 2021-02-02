188c223e31f8d7fb1cedfd7000e9f1245b29be9e
Mfweb <h@i2s.io>
2021-02-02 12:16:09 +0800

    警告内容过滤HTML

--- AdaoMemberSystem/utils/http.js
+++ AdaoMemberSystem/utils/http.js
@@ -1,41 +1,39 @@
 const Promise = require('./es6-promise.js');
-const wxApis = require('./wxApis.js');
 /**
  * @brief 保存所有Cookie
  * @param data:本次请求的setcookie内容
  * @retval None
  */
 function save_cookie(data) {
-  data = data.replace(" ", "");
-  data = data.split(";");
-  var saved_data = wx.getStorageSync('user_cookie') || {};
-  //兼容之前登录过的用户
-  if (typeof saved_data == 'string') {
-    wx.setStorageSync('user_cookie', {});
-    saved_data = {};
-  }
-  var save_array = Array();
-  for (let i = 0; i < data.length; i++) {
-    var temp_data = data[i].split("=");
-    saved_data[temp_data[0]] = temp_data[1];
-  }
-  wx.setStorageSync('user_cookie', saved_data);
+    data = data.replace(" ", "");
+    data = data.split(";");
+    let saved_data = wx.getStorageSync('user_cookie') || {};
+    //兼容之前登录过的用户
+    if (typeof saved_data == 'string') {
+        wx.setStorageSync('user_cookie', {});
+        saved_data = {};
+    }
+    for (let i = 0; i < data.length; i++) {
+        let temp_data = data[i].split("=");
+        saved_data[temp_data[0]] = temp_data[1];
+    }
+    wx.setStorageSync('user_cookie', saved_data);
 }
 /**
  * @brief 获取所有Cookie
  * @retval cookie内容
  */
 function get_cookie() {
-  var cookies = wx.getStorageSync('user_cookie') || {};
-  var out_str = '';
-  for (let o in cookies) {
-    if (o == 'path') continue;
-    out_str += o;
-    out_str += '=';
-    out_str += cookies[o];
-    out_str += ';';
-  }
-  return out_str;
+    let cookies = wx.getStorageSync('user_cookie') || {};
+    let out_str = '';
+    for (let o in cookies) {
+        if (o == 'path') continue;
+        out_str += o;
+        out_str += '=';
+        out_str += cookies[o];
+        out_str += ';';
+    }
+    return out_str;
 }
 /**
  * @brief 获取指定key的Cookie内容
@@ -43,8 +41,8 @@ function get_cookie() {
  * @retval 指定Key的内容
  */
 function get_cookie_key(key) {
-  var cookies = wx.getStorageSync('user_cookie') || {};
-  return cookies[key];
+    let cookies = wx.getStorageSync('user_cookie') || {};
+    return cookies[key];
 }
 /**
  * @brief 设置指定key的cookie内容
@@ -53,12 +51,12 @@ function get_cookie_key(key) {
  * @retval None
  */
 function set_cookie_key(key, value) {
-  var cookies = wx.getStorageSync('user_cookie') || {};
-  if (cookies.hasOwnProperty(key))
-    cookies[key] = value;
-  else
-    cookies.push({ key: value });
-  wx.setStorageSync('user_cookie', cookies);
+    let cookies = wx.getStorageSync('user_cookie') || {};
+    if (cookies.hasOwnProperty(key))
+        cookies[key] = value;
+    else
+        cookies.push({ key: value });
+    wx.setStorageSync('user_cookie', cookies);
 }
 
 /**
@@ -67,34 +65,34 @@ function set_cookie_key(key, value) {
  * @param pdata:POST数据
  * @retval None
  */
-function request (url, data) {
-  return new Promise((resolve, reject) => {
-    wx.request({
-      url,
-      data,
-      method: 'POST',
-      header: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        //      'User-Agent': 'HavfunClient-WeChatAPP',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Cookie': get_cookie()
-      },
-      success: function (res) {
-        if (res.statusCode >= 400) {
-          console.error(`${url} ${JSON.stringify(res)}`);
-          reject(res);
-          return;
-        }
-        if (res && res.header && res.header['Set-Cookie']) {
-          save_cookie(res.header['Set-Cookie']);
-        }
-        resolve(res);
-      },
-      fail: function () {
-        reject(false);
-      }
+function request(url, data) {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url,
+            data,
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                //      'User-Agent': 'HavfunClient-WeChatAPP',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Cookie': get_cookie()
+            },
+            success: function (res) {
+                if (res.statusCode >= 400) {
+                    console.error(`${url} ${JSON.stringify(res)}`);
+                    reject(res);
+                    return;
+                }
+                if (res && res.header && res.header['Set-Cookie']) {
+                    save_cookie(res.header['Set-Cookie']);
+                }
+                resolve(res);
+            },
+            fail: function () {
+                reject(false);
+            }
+        });
     });
-  });
 }
 
 /**
@@ -104,76 +102,76 @@ function request (url, data) {
  * @retval None
  */
 function requestGet(url, data = {}) {
-  return new Promise((resolve, reject) => {
-    wx.request({
-      url,
-      data,
-      method: 'GET',
-      success: function (res) {
-        if (res.statusCode >= 400) {
-          reject(res);
-          return;
-        }
-        resolve(res);
-      },
-      fail: function () {
-        reject(false);
-      }
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url,
+            data,
+            method: 'GET',
+            success: function (res) {
+                if (res.statusCode >= 400) {
+                    reject(res);
+                    return;
+                }
+                resolve(res);
+            },
+            fail: function () {
+                reject(false);
+            }
+        });
     });
-  });
 }
 
 /**
  * 下载文件
  */
 function downloadFile(url, cookie = null) {
-  return new Promise((resolve, reject) => {
-    wx.downloadFile({
-      url,
-      header: {
-        'Cookie': cookie
-      },
-      success: function (res) {
-        resolve(res);
-      },
-      fail: function () {
-        reject();
-      }
+    return new Promise((resolve, reject) => {
+        wx.downloadFile({
+            url,
+            header: {
+                'Cookie': cookie
+            },
+            success: function (res) {
+                resolve(res);
+            },
+            fail: function () {
+                reject();
+            }
+        });
     });
-  });
 }
 
 /**
  * @brief 下载验证码
  */
 function get_verifycode() {
-  const app = getApp();
-  return new Promise((resolve, reject) => {
-    wx.downloadFile({
-      url: app.globalData.ApiUrls.VerifyCodeURL + "?code=" + Math.random(),
-      header: {
-        'Cookie': get_cookie()
-      },
-      success: function (res) {
-        if (res.errMsg != 'downloadFile:ok') {
-          reject('loaderror.png');
-          return;
-        }
-        resolve(res.tempFilePath);
-      },
-      fail: function () {
-        reject('loaderror.png');
-      }
+    const app = getApp();
+    return new Promise((resolve, reject) => {
+        wx.downloadFile({
+            url: app.globalData.ApiUrls.VerifyCodeURL + "?code=" + Math.random(),
+            header: {
+                'Cookie': get_cookie()
+            },
+            success: function (res) {
+                if (res.errMsg != 'downloadFile:ok') {
+                    reject('loaderror.png');
+                    return;
+                }
+                resolve(res.tempFilePath);
+            },
+            fail: function () {
+                reject('loaderror.png');
+            }
+        });
     });
-  });
-  
+
 }
 
 module.exports = {
-  request: request,
-  requestGet: requestGet,
-  downloadFile: downloadFile,
-  get_cookie_key: get_cookie_key,
-  set_cookie_key: set_cookie_key,
-  get_verifycode: get_verifycode
+    request,
+    requestGet,
+    downloadFile,
+    get_cookie_key,
+    set_cookie_key,
+    get_verifycode
 }
--- AdaoMemberSystem/utils/wxApis.js
+++ AdaoMemberSystem/utils/wxApis.js
@@ -4,157 +4,156 @@ import drawQrcode from './weapp.qrcode.min.js'
  * 保存文件
  */
 export const saveFile = (filePath) => {
-  return new Promise((resolve, reject) => {
-    wx.saveFile({
-      tempFilePath: filePath,
-      success: function (res) {
-        resolve(res);
-      },
-      fail: function () {
-        reject();
-      }
+    return new Promise((resolve, reject) => {
+        wx.saveFile({
+            tempFilePath: filePath,
+            success: function (res) {
+                resolve(res);
+            },
+            fail: function () {
+                reject();
+            }
+        });
     });
-  });
 }
 
 
 /**
- * 将异步保存转同步
  * 之所以不直接用同步保存是因为同步保存不一定100%能成功
  */
 export const setStorage = (key, data) => {
-  return new Promise((resolve, reject) => {
-    wx.setStorage({
-      key: key,
-      data: data,
-      success: function () {
-        resolve();
-      },
-      fail: function () {
-        reject();
-      }
+    return new Promise((resolve, reject) => {
+        wx.setStorage({
+            key: key,
+            data: data,
+            success: function () {
+                resolve();
+            },
+            fail: function () {
+                reject();
+            }
+        });
     });
-  });
 }
 
 /**
  * 获取保存文件列表
  */
 export const getSavedFileList = () => {
-  return new Promise((resolve, reject) => {
-    wx.getSavedFileList({
-      success: function (res) {
-        resolve(res);
-      },
-      fail: function () {
-        reject();
-      }
+    return new Promise((resolve, reject) => {
+        wx.getSavedFileList({
+            success: function (res) {
+                resolve(res);
+            },
+            fail: function () {
+                reject();
+            }
+        });
     });
-  });
 }
 
 /**
  * 延时
  */
 export const setTimeoutX = (ms) => {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve();
-    }, ms);
-  });
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve();
+        }, ms);
+    });
 }
 
 /**
  * 绘制二维码
  */
 export const drawQrcodeX = (id, content, _this) => {
-  return new Promise(function (resolve, reject) {
-    drawQrcode({
-      width: 200,
-      height: 200,
-      canvasId: id,
-      text: content,
-      _this: _this,
-      callback: function () {
-        resolve();
-      }
+    return new Promise(function (resolve, reject) {
+        drawQrcode({
+            width: 200,
+            height: 200,
+            canvasId: id,
+            text: content,
+            _this: _this,
+            callback: function () {
+                resolve();
+            }
+        });
     });
-  });
 }
 
 /**
  * 获取canvas数据
  */
 export const canvasGetImageData = (id, _this) => {
-  return new Promise(function (resolve, reject) {
-    wx.canvasGetImageData({
-      canvasId: id,
-      x: 0,
-      y: 0,
-      width: 200,
-      height: 200,
-      success: function (res) {
-        resolve(res);
-      },
-      fail: function () {
-        reject();
-      }
-    }, _this);
-  });
+    return new Promise(function (resolve, reject) {
+        wx.canvasGetImageData({
+            canvasId: id,
+            x: 0,
+            y: 0,
+            width: 200,
+            height: 200,
+            success: function (res) {
+                resolve(res);
+            },
+            fail: function () {
+                reject();
+            }
+        }, _this);
+    });
 }
 
 /**
  * 写入canvas数据
  */
 export const canvasPutImageData = (id, data, _this) => {
-  return new Promise(function (resolve, reject) {
-    wx.canvasPutImageData({
-      canvasId: id,
-      data: data,
-      x: 10,
-      y: 10,
-      width: 200,
-      success: function () {
-        resolve();
-      },
-      fail: function () {
-        reject();
-      }
-    }, _this);
-  });
+    return new Promise(function (resolve, reject) {
+        wx.canvasPutImageData({
+            canvasId: id,
+            data: data,
+            x: 10,
+            y: 10,
+            width: 200,
+            success: function () {
+                resolve();
+            },
+            fail: function () {
+                reject();
+            }
+        }, _this);
+    });
 }
 
 /**
  * 保存canvas内容
  */
 export const canvasToTempFilePath = (id, _this) => {
-  return new Promise(function (resolve, reject) {
-    wx.canvasToTempFilePath({
-      canvasId: id,
-      success: function (res) {
-        resolve(res);
-      },
-      fail: function () {
-        reject();
-      }
-    }, _this);
-  });
+    return new Promise(function (resolve, reject) {
+        wx.canvasToTempFilePath({
+            canvasId: id,
+            success: function (res) {
+                resolve(res);
+            },
+            fail: function () {
+                reject();
+            }
+        }, _this);
+    });
 }
 
 /**
  * 获取微信运动步数
  */
 export const getWeRunData = () => {
-  return new Promise(function (resolve, reject) {
-    wx.getWeRunData({
-      success: function (e) {
-        resolve(e);
-      },
-      fail: function() {
-        reject();
-      }
+    return new Promise(function (resolve, reject) {
+        wx.getWeRunData({
+            success: function (e) {
+                resolve(e);
+            },
+            fail: function () {
+                reject();
+            }
+        });
     });
-  });
 }
 
 
@@ -162,36 +161,36 @@ export const getWeRunData = () => {
  * 微信登录
  */
 export const login = () => {
-  return new Promise(function (resolve, reject) {
-    wx.login({
-      success: function (e) {
-        resolve(e);
-      },
-      fail: function () {
-        reject();
-      }
+    return new Promise(function (resolve, reject) {
+        wx.login({
+            success: function (e) {
+                resolve(e);
+            },
+            fail: function () {
+                reject();
+            }
+        });
     });
-  });
 }
 
 /**
  * 微信登录
  */
 export const authorize = () => {
-  return new Promise(function (resolve, reject) {
-    wx.authorize({
-      scope: 'scope.werun',
-      success: function (e) {
-        if (e.errMsg == "authorize:ok") {
-          resolve(e);
-        }
-        else {
-          reject();
-        }
-      },
-      fail: function () {
-        reject();
-      }
+    return new Promise(function (resolve, reject) {
+        wx.authorize({
+            scope: 'scope.werun',
+            success: function (e) {
+                if (e.errMsg == "authorize:ok") {
+                    resolve(e);
+                }
+                else {
+                    reject();
+                }
+            },
+            fail: function () {
+                reject();
+            }
+        });
     });
-  });
 }
