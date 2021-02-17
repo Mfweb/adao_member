53c38dc4500cdfd587ee5728995339a8b8e88c47
Mfweb <h@i2s.io>
2021-02-18 03:10:51 +0800

    调整菜单结构

--- AdaoMemberSystem/pages/userMember/userMember.js
+++ AdaoMemberSystem/pages/userMember/userMember.js
@@ -2,7 +2,7 @@ const app = getApp();
 
 Page({
     data: {
-        pageIndex: 0,
+        pageIndex: 'cookie',
         statusBarHeight: app.globalData.SystemInfo.Windows.statusBarHeight,
 
         startLoadCookies: false,
@@ -26,7 +26,7 @@ Page({
      */
     resetData: function () {
         this.setData({
-            pageIndex: 0,
+            pageIndex: 'cookie',
             statusBarHeight: app.globalData.SystemInfo.Windows.statusBarHeight,
 
             startLoadCookies: false,
@@ -45,47 +45,81 @@ Page({
                 show: false,
                 userIco: 'http://adnmb.com/Public/member/users/assets/img/user_nohead.png',
                 statusBarHeight: app.globalData.SystemInfo.Windows.statusBarHeight,
-                selectedIndex: 0,
+                selectedIndex: 'cookie',
                 picURL: '',
                 userName: '匿名肥宅',
                 appList: app.globalData.AppList.iOS.concat(app.globalData.AppList.Android),
-                menuList: [{
-                    name: '饼干管理',
-                    icon: 'cookie',
-                    canSwitch: true
-                }, {
-                    name: '实名认证',
-                    icon: 'certified',
-                    canSwitch: true
-                }, {
-                    name: '密码修改',
-                    icon: 'passwd',
-                    canSwitch: true
-                }, {
-                    name: '肥宅排行',
-                    icon: 'sport',
-                    canSwitch: true
-                }, {
-                    name: '选老婆',
-                    icon: 'wifes',
-                    canSwitch: true
-                }, {
-                    name: '芦苇表情包',
-                    icon: 'lw',
-                    canSwitch: true
-                }, {
-                    name: '关于',
-                    icon: 'about',
-                    canSwitch: false
-                }, {
-                    name: '退出',
-                    icon: 'exit',
-                    canSwitch: false
-                },
-                ]
+                menuList: {
+                    cookie: {
+                        name: '饼干管理',
+                        icon: 'cookie',
+                        canSwitch: true,
+                        events: {
+                            onPullDownRefresh: () => this.setData({ startLoadCookies: true }),
+                            onReachBottom: null
+                        }
+                    }, certified: {
+                        name: '实名认证',
+                        icon: 'certified',
+                        canSwitch: true,
+                        events: {
+                            onPullDownRefresh: () => this.setData({ startLoadAuth: true }),
+                            onReachBottom: null
+                        }
+                    }, sport: {
+                        name: '肥宅排行',
+                        icon: 'sport',
+                        canSwitch: true,
+                        events: {
+                            onPullDownRefresh: () => this.setData({ startLoadSport: true }),
+                            onReachBottom: null
+                        }
+                    }, wifes: {
+                        name: '选老婆',
+                        icon: 'wifes',
+                        canSwitch: true,
+                        events: {
+                            onPullDownRefresh: () => this.setData({ startReloadWife: true }),
+                            onReachBottom: () => this.setData({ startLoadWife: true })
+                        }
+                    }, lwmeme: {
+                        name: '芦苇表情包',
+                        icon: 'lw',
+                        canSwitch: true,
+                        events: {
+                            onPullDownRefresh: () => wx.stopPullDownRefresh(),
+                            onReachBottom: null
+                        }
+                    }, about: {
+                        name: '关于',
+                        icon: 'about',
+                        canSwitch: false,
+                        events: {
+                            onPullDownRefresh: null,
+                            onReachBottom: null
+                        }
+                    }, password: {
+                        name: '密码修改',
+                        icon: 'passwd',
+                        canSwitch: true,
+                        events: {
+                            onPullDownRefresh: () => wx.stopPullDownRefresh(),
+                            onReachBottom: null
+                        }
+                    }, exit: {
+                        name: '退出',
+                        icon: 'exit',
+                        canSwitch: false,
+                        events: {
+                            onPullDownRefresh: null,
+                            onReachBottom: null
+                        }
+                    }
+                }
             }
         });
     },
+
     /**
      * 页面渲染完成
      */
@@ -99,36 +133,23 @@ Page({
         }
         this.setData({ 'popupMenuOpenData.userName': tempUserName });
     },
+
     /**
      * 开始下拉刷新
      */
     onPullDownRefresh: function () {
-        if (this.data.pageIndex == 0) {
-            //处理饼干数据
-            this.setData({
-                startLoadCookies: true
-            });
-        }
-        else if (this.data.pageIndex == 1) {
-            //处理实名认证相关数据
-            this.setData({
-                startLoadAuth: true
-            });
-        }
-        else if (this.data.pageIndex == 2 || this.data.pageIndex == 5) {
-            wx.stopPullDownRefresh();
-        }
-        else if (this.data.pageIndex == 3) {
-            this.setData({
-                startLoadSport: true
-            });
-        }
-        else if (this.data.pageIndex == 4) {
-            this.setData({
-                startReloadWife: true
-            });
-        }
+        if (this.data.popupMenuOpenData.menuList[this.data.pageIndex].events.onPullDownRefresh !== null)
+            this.data.popupMenuOpenData.menuList[this.data.pageIndex].events.onPullDownRefresh();
     },
+
+    /**
+     * 上拉加载更多
+     */
+    onReachBottom: function () {
+        if (this.data.popupMenuOpenData.menuList[this.data.pageIndex].events.onReachBottom !== null)
+            this.data.popupMenuOpenData.menuList[this.data.pageIndex].events.onReachBottom();
+    },
+
     /**
      * 下拉刷新所有
      * 不会有下拉动画
@@ -140,45 +161,41 @@ Page({
             startLoadSport: true
         });
     },
-    /**
-     * 上拉加载更多
-     */
-    onReachBottom: function () {
-        if (this.data.pageIndex == 4) {
-            this.setData({
-                startLoadWife: true
-            });
-        }
-    },
+
     /**
      * 页面改变
+     * @param {Object} event
      */
-    onChangePage: function (id) {
-        switch (parseInt(id.detail)) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                this.setData({ pageIndex: id.detail });
+    onChangePage: function (event) {
+        switch (event.detail) {
+            case 'cookie':
+            case 'certified':
+            case 'sport':
+            case 'wifes':
+            case 'lwmeme':
+            case 'password':
+                this.setData({ pageIndex: event.detail });
                 wx.pageScrollTo({
                     duration: 0,
                     selector: '#top'
                 });
                 break;
-            case 6:
+            case 'about':
                 wx.navigateTo({
                     url: '../about/about',
                 });
                 break;
-            case 7:
+            case 'exit':
                 app.logOut();
                 break;
             default:
                 app.showError('哈？');
         }
     },
+    /**
+     * 开始加载数据
+     * @param {Object} event 
+     */
     onLoadStart: function (event) {
         switch (event.detail.from) {
             case 'auth':
@@ -195,6 +212,10 @@ Page({
                 break;
         }
     },
+    /**
+     * 加载数据结束
+     * @param {Object} event 
+     */
     onLoadEnd: function (event) {
         switch (event.detail.from) {
             case 'auth':
